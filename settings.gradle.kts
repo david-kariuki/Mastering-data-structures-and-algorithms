9e1106cc4a728c31c1eb7ae1219916337ff70379
David Kariuki (DK) <14153276+david-kariuki@users.noreply.github.com>
2026-03-23 10:57:54 +0300

    Revert "config: Project setup"
    This reverts commit d7204f5bdec6d7afbf6f58a6a53f1fcb63d0ae28.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "mastering-data-structures-and-algorithms"
+include("udemy-elshad-karimov-master-dsa-java")
 include("practice_data_structures_and_algorithms")
-include("learn-dsa")
