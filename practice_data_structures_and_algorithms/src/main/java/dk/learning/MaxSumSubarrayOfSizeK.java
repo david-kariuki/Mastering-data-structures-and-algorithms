4977528b418082950ebc620a420814152a70b608
David Kariuki (DK) <14153276+david-kariuki@users.noreply.github.com>
2026-03-23 10:57:54 +0300

    Revert "chore: Practising data structures and algorithms"
    This reverts commit c2e3e45658e76c3d8862d5e9420128e85b08c468.

--- practice_data_structures_and_algorithms/src/main/java/dk/learning/MaxSumSubarrayOfSizeK.java
+++ practice_data_structures_and_algorithms/src/main/java/dk/learning/MaxSumSubarrayOfSizeK.java
@@ -39,40 +39,23 @@ public class MaxSumSubarrayOfSizeK {
         System.out.println("Maximum sum in array " + Arrays.toString(array) + " is " + maxSumSubarrayOfSizeK(array, k)); // 6
     }
 
-    /**
-     * Finding the maximum sum of a contiguous subarray of size k.
-     * <p>
-     * To solve this problem, we can use the sliding window technique. We will maintain a window of size k and
-     * calculate the sum of the elements in that window.
-     * <p>
-     * As we slide the window to the right, we will subtract the element that is leaving the window and add the new
-     * element that is entering the window. This way, we can efficiently compute the sum for each subarray of size
-     * k without recalculating from scratch.
-     *
-     * @param nums Array
-     * @param k    Size of the subarray
-     * @return Maximum sum of a contiguous subarray of size k
-     * Time Complexity: O(n) where n is the length of the input array. We traverse the array once to calculate the
-     * initial sum and then once more to slide the window across the array.
-     * Space Complexity: O(1) since we are using only a constant amount of extra space to store the current window
-     * sum and the largest sum.
-     */
+
     private static int maxSumSubarrayOfSizeK(int[] nums, int k) {
         int windowSum = 0;
 
-        // Calculate the sum of the first window of size k
-        for (int i = 0; i < k; i++) {                       //  ---------------- O(k)
-            windowSum += nums[i];                           // ---------------- O(1)
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
         }
 
-        int largestSum = windowSum;                         // ---------------- O(1)
+        int largestSum = windowSum;
 
-        for (int right = k; right < nums.length; right++) { // ---------------- O(n - k)
-            int left = right - k;                           // ---------------- O(1)
-            windowSum -= nums[left];                        // ---------------- O(1)
-            windowSum += nums[right];                       // ---------------- O(1)
-            largestSum = Math.max(largestSum, windowSum);   // ---------------- O(1)
+        for (int right = k; right < nums.length; right++) {
+            int left = right - k;
+            windowSum -= nums[left];
+            windowSum += nums[right];
+            largestSum = Math.max(largestSum, windowSum);
         }
-        return largestSum;                                  // ---------------- O(1)
+
+        return largestSum;
     }
 }
